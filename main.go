38e5d44e18ad07f0d72c955df1946ed7cab655a6
Ingmar Drewing <ingmar@drewing.de>
2018-05-02 14:32:04 +0200

    Add tag slice generation

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+import "github.com/ingmardrewing/gomicSocMedShared"
+
+type Content struct {
+	gomicSocMedShared.Content
+}
+
+func main() {
+	fmt.Println("vim-go")
+}
